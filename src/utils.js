23d7c37c3ba64ee361904a3452f3f03bbd919658
Florian Morel <ayamflow@gmail.com>
2014-12-10 15:09:38 +0100

    Add isString/isFunction to utils

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,11 @@
 module.exports = {
     hasOwnProp: Object.prototype.hasOwnProperty,
     toString: Object.prototype.toString,
-    concat: Array.prototype.concat
+    concat: Array.prototype.concat,
+    isString: function(str) {
+      return this.toString.call(str).toLowerCase() == "[object string]";
+    },
+    isFunction: function(fn) {
+      return this.toString.call(fn).toLowerCase() == "[object function]";
+    }
 };
